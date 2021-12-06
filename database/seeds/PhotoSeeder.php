6f13834b541e20ae01262a60638bfac552389aa4
Ivanpepp <nastroivan3@gmail.com>
2021-12-06 12:20:30 +0100

    photo seeded

--- database/seeds/PhotoSeeder.php
+++ database/seeds/PhotoSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Faker\Generator as Faker;
+use App\Models\Apartment;
+use App\Models\Photo;
 
 class PhotoSeeder extends Seeder
 {
@@ -9,8 +13,15 @@ class PhotoSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $apartment_ids = Apartment::pluck('id')->toArray();
+
+        for ($i = 0 ; $i < 500 ; $i++){
+            $newPhoto = new Photo();
+            $newPhoto->apartment_id = Arr::random($apartment_ids);
+            $newPhoto->image_thumb = $faker->imageUrl(400, 400, $newPhoto->apartment->title, true);
+            $newPhoto->save();
+        }
     }
 }
