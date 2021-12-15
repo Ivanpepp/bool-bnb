2b0a5587053ccafa146a3a84e5f447c370e9171d
Ivanpepp <nastroivan3@gmail.com>
2021-12-15 10:54:38 +0100

    token generated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Braintree\Gateway;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(Gateway::class, function($app){
+            return new Gateway(
+                [
+                    'environment' => 'sandbox',
+                    'merchantId' => 'n8sd473h7rf697yq',
+                    'publicKey' => 'wmntb74wjstyrpb3',
+                    'privateKey' => '7aafd8d9e089ebca31612a229cd70c38',
+                ]
+            );
+        });
     }
 }
