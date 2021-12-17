c2c730bbe3367e2de27bef0f4914f045cf68fe55
Jack-rm <jackcnsr94@gmail.com>
2021-12-17 13:55:12 +0100

    Header change-color function + toggler

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,71 +96,192 @@
                     </div>
                 </div>
             </nav> -->
-            <nav class="navbar navbar-expand-lg fixed-top navbar-dark" style="background-color: #131313">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Bool<span class="text-primary">BNB</span>
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+            <!-- <div class="collapse pt-4" id="navbarSupportedContent" style="background-color: #131313">
+                <ul class="navbar-nav ml-auto text-center pt-5">
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link text-light p-3" href="{{ route('guest.home') }}">
+                            <span class="h3">Annunci | </span><i class="fas fa-search fa-2x"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-light p-3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <span class="h3">Profilo | </span><i class="fas fa-house-user fa-2x "></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{route('login')}}">
+                                {{ __('Login') }}
+                            </a>
+                            @if (Route::has('register'))
+                            <a class="dropdown-item" href="{{route('register')}}">
+                                {{ __('Registrati') }}
+                            </a>
+                        </div>
+                    </li>
+                    @endif
+                    @else
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="{{ route('guest.home') }}"><i class="fas fa-search fa-lg"></i> | Annunci</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="{{ route('host.mail.index') }}"><i class="far fa-envelope fa-lg"></i></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="user-nav nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} |
+                            <i class="fas fa-house-user fa-lg"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{route('host.home')}}">
+                                {{ __('Dashboard') }}
+                            </a>
+                            <a class="dropdown-item" href="{{route('host.apartments.index')}}">
+                                {{ __('Apartments') }}
+                            </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-                    <div class="collapse navbar-collapse text-center" id="navbarSupportedContent" style="background-color: #131313">
-                        <ul class="navbar-nav ml-auto">
-                            @guest
-                            <li class="nav-item">
-                                <a class="nav-link text-light p-3" href="{{ route('guest.home') }}"><i class="fas fa-search fa-2x"></i></a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-light p-3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <i class="fas fa-house-user fa-2x"></i>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{route('login')}}">
-                                        {{ __('Login') }}
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
+                </ul>
+            </div> -->
+            <!-- <nav class="navbar navbar-expand-sm fixed-top navbar-dark nav-active" style="background-color: #131313">
+                <div class="container"> -->
+            <!-- <a class="navbar-brand" href="{{ url('/') }}">
+                    Bool<span class="text-primary">BNB</span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mycollapse" aria-controls="mycollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button> -->
+            <div class="pos-f-t">
+                <nav class="navbar navbar-expand-sm navbar-dark fixed-top">
+                    <div class="container">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            Bool<span class="my_blue">BNB</span>
+                        </a>
+                        <button class="navbar-toggler text-light" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="fas fa-bars fa-lg p-2"></i>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="mycollapse">
+                            <ul class="navbar-nav ml-auto text-center">
+                                @guest
+                                <li class="nav-item">
+                                    <a class="nav-link text-light p-3" href="{{ route('guest.home') }}">
+                                        <i class="fas fa-search fa-2x"></i>
                                     </a>
-                                    @if (Route::has('register'))
-                                    <a class="dropdown-item" href="{{route('register')}}">
-                                        {{ __('Registrati') }}
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle text-light p-3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <i class="fas fa-house-user fa-2x my_blue"></i>
                                     </a>
-                                </div>
-                            </li>
-                            @endif
-                            @else
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="{{ route('guest.home') }}"><i class="fas fa-search fa-lg"></i> | Annunci</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-light" href="{{ route('host.mail.index') }}"><i class="far fa-envelope fa-lg"></i></a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="user-nav nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} |
-                                    <i class="fas fa-house-user fa-lg"></i>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{route('host.home')}}">
-                                        {{ __('Dashboard') }}
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{route('login')}}">
+                                            {{ __('Login') }}
+                                        </a>
+                                        @if (Route::has('register'))
+                                        <a class="dropdown-item" href="{{route('register')}}">
+                                            {{ __('Registrati') }}
+                                        </a>
+                                    </div>
+                                </li>
+                                @endif
+                                @else
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" href="{{ route('guest.home') }}"><i class="fas fa-search fa-2x"></i></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-light" href="{{ route('host.mail.index') }}"><i class="far fa-envelope fa-2x"></i></a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="user-nav nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} |
+                                        <i class="fas fa-house-user fa-2x my_blue"></i>
                                     </a>
-                                    <a class="dropdown-item" href="{{route('host.apartments.index')}}">
-                                        {{ __('Apartments') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{route('host.home')}}">
+                                            {{ __('Dashboard') }}
+                                        </a>
+                                        <a class="dropdown-item" href="{{route('host.apartments.index')}}">
+                                            {{ __('Apartments') }}
+                                        </a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                                @endguest
+                            </ul>
+                        </div>
+                        <div class="collapse my_hidden my_black" id="navbarToggleExternalContent" style="width: 100%">
+                            <div class="p-2 text-center">
+                                @guest
+                                <a class="text-light toggler-link" href="{{ route('guest.home') }}">
+                                    <span class="h4">Annunci</span>
+                                </a>
+                                <div class="dropdown pt-3">
+                                    <a id="navbarDropdownMenuLink" class="dropdown-toggle p-3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <span class="h4 my_blue">Profilo Utente</span>
                                     </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <a class="dropdown-item" href="{{route('login')}}">
+                                            {{ __('Login') }}
+                                        </a>
+                                        @if (Route::has('register'))
+                                        <a class="dropdown-item" href="{{route('register')}}">
+                                            {{ __('Registrati') }}
+                                        </a>
+                                    </div>
                                 </div>
-                            </li>
-                            @endguest
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+                                @endif
+                                @else
+                                <a class="text-light toggler-link" href="{{ route('guest.home') }}">
+                                    <span class="h4">Esplora</span>
+                                </a>
+                                <br>
+                                <a class="text-light toggler-link-2" href="{{ route('host.mail.index') }}">
+                                    <span class="h4">Messaggi</span>
+                                </a>
+                                <br>
+                                <a class="text-light toggler-link-3" href="{{ route('host.apartments.index') }}">
+                                    <span class="h4">Annunci Personali</span>
+                                </a>
+                                <br>
+                                <div class="dropdown pt-3">
+                                    <a id="navbarDropdown" class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <span class="h4">{{ Auth::user()->name }}</span>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{route('host.home')}}">
+                                            {{ __('Dashboard') }}
+                                        </a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                 document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
 
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </div>
+                                @endguest
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
 
         <main>
@@ -171,6 +292,52 @@
     </div>
 </body>
 <script>
+    const navbar = document.querySelector('.fixed-top');
+    const hiddenNav = document.querySelector('.my_hidden');
+    const toggler = document.querySelector('.fa-bars');
+    const menuToggler = document.querySelector('#navbarToggleExternalContent');
+    const linkToggler = document.querySelector('.toggler-link');
+    const linkToggler2 = document.querySelector('.toggler-link-2');
+    const linkToggler3 = document.querySelector('.toggler-link-3');
+    window.onscroll = () => {
+        if (window.scrollY > 80) {
+            navbar.classList.add('nav-active');
+            navbar.classList.remove('navbar-dark');
+            navbar.classList.add('navbar-light');
+            hiddenNav.classList.remove('show');
+            toggler.classList.add('my_blue');
+            menuToggler.classList.remove('my_black');
+            menuToggler.classList.add('my_white');
+            linkToggler.classList.remove('text-light');
+            linkToggler.classList.add('text-dark');
+            linkToggler2.classList.remove('text-light');
+            linkToggler2.classList.add('text-dark');
+            linkToggler3.classList.remove('text-light');
+            linkToggler3.classList.add('text-dark');
+
+            // } else if (window.scrollY <= 80){
+            //     navbar.classList.add('text-light');
+        } else {
+            navbar.classList.remove('nav-active');
+            navbar.classList.add('navbar-dark');
+            navbar.classList.remove('navbar-light');
+            toggler.classList.remove('my_blue');
+            menuToggler.classList.remove('my_white');
+            menuToggler.classList.add('my_black');
+            linkToggler.classList.remove('text-dark');
+            linkToggler.classList.add('text-light');
+            linkToggler2.classList.remove('text-dark');
+            linkToggler2.classList.add('text-light');
+            linkToggler3.classList.remove('text-dark');
+            linkToggler3.classList.add('text-light')
+        }
+    };
+
+    window.onresize = () => {
+        if (window.innerWidth >= 576) {
+            hiddenNav.classList.remove('show')
+        }
+    };
 </script>
 
 </html>
