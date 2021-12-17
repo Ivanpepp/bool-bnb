b2d9238ccb8811bf6db8afff8e47618282e4176d
WalterFrongia <walter.frongia@libero.it>
2021-12-17 17:38:31 +0100

    Aggiunto in welcome jumbotron per utenti loggati

--- public/js/front.js
+++ public/js/front.js
@@ -16873,7 +16873,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Aorus\Documents\Boolean\final_project\bool-bnb\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Dev\Boolean\bool-bnb\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,6 +37,7 @@
 
                     <div class="col-12 text-center" >
                         <div class="shadow-lg" id="my-section-login">
+                            @guest
                                 <div class="text-left pl-3 my-text">
                                     <div class="col-sm-7 col-lg-6 pt-5">
                                         <h2>ENTRA ANCHE TU A FAR PARTE DEL MONDO DI BOOLBNB</h2>
@@ -46,6 +47,17 @@
                                     </div>
                                     <a href="{{ route('register') }}" class="my-btn-homepage ml-3">REGISTRATI</a>
                                 </div>
+                            @else
+                                <div class="text-left pl-3 my-text">
+                                    <div class="col-sm-7 col-lg-6 pt-5">
+                                        <h2>ENTRA ANCHE TU A FAR PARTE DEL MONDO DI BOOLBNB</h2>
+                                    </div>
+                                    <div class="col-sm-8 col-lg-5 pt-2 pb-2">
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus soluta vel, facere eligendi sit voluptas ipsum unde nihil sed, officia provident qui sint accusantium ducimus sequi quia atque, illo ex?</p>
+                                    </div>
+                                    <a href="{{ route('host.apartments.create') }}" class="my-btn-homepage ml-3">AGGIUNGI UN APPARTAMENTO</a>
+                                </div>
+                            @endguest
                         </div>
                     </div>
                 </div>         
