056825b1b3c52042ae488c0f453cd2d75f613449
WalterFrongia <walter.frongia@libero.it>
2021-12-20 21:41:19 +0100

    Modificate img in vue component in apartamentCard component

--- public/js/front.js
+++ public/js/front.js
@@ -2335,6 +2335,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'ApartmentCard',
   props: ['apartment', 'baseUri', 'photos'],
@@ -2482,7 +2484,7 @@ __webpack_require__.r(__webpack_exports__);
       });
     }
     /*   kmCoverter(lat1,lon1){
-             // generally used geo measurement function
+               // generally used geo measurement function
               let R = 6378.137; // Radius of earth in KM
               let dLat = lat1 * Math.PI / 180;
               let dLon = lon1 * Math.PI / 180;
@@ -2686,7 +2688,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, ".my-card[data-v-4662a702] {\n  padding: 10px;\n  border-radius: 10px;\n}\n.my-card img[data-v-4662a702] {\n  width: 90%;\n  height: 90%;\n  border-radius: 15px;\n}", ""]);
+exports.push([module.i, ".my-card[data-v-4662a702] {\n  padding: 10px;\n  border-radius: 10px;\n}\n.my-card .my-img-container[data-v-4662a702] {\n  height: 200px;\n  width: 350px;\n}\n.my-card .my-img-container img[data-v-4662a702] {\n  width: 100%;\n  height: 100%;\n  background-size: cover;\n  border-radius: 15px;\n}", ""]);
 
 // exports
 
@@ -2724,7 +2726,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n#map {\n  height: 650px;\n  width: 100%;\n}\n.search-container{\n    position: relative;\n}\n#query{\n    position: absolute;\n    top: 100px;\n    left: 40px;\n    z-index: 99;\n    border: none;\n}\n#query:focus{\n    outline: none !important;\n    box-shadow: 0 0 10px #719ECE;\n}\n", ""]);
+exports.push([module.i, "\n#map {\r\n  height: 650px;\r\n  width: 100%;\n}\n.search-container{\r\n    position: relative;\n}\n#query{\r\n    position: absolute;\r\n    top: 100px;\r\n    left: 40px;\r\n    z-index: 99;\r\n    border: none;\n}\n#query:focus{\r\n    outline: none !important;\r\n    box-shadow: 0 0 10px #719ECE;\n}\r\n", ""]);
 
 // exports
 
@@ -3926,27 +3928,37 @@ var render = function () {
   var _c = _vm._self._c || _h
   return _c(
     "div",
-    { staticClass: "col-xs-12 col-sm-6 col-lg-4 mb-2 my-card ivan-card" },
+    { staticClass: "col-xs-12 col-sm-6 col-lg-4 mb-3 my-card ivan-card" },
     [
-      _c("img", {
-        staticClass: "img-fluid",
-        attrs: {
-          src:
-            "http://127.0.0.1:8000/storage/apartments/images/" +
-            _vm.photoIds[0],
-          alt: "",
-        },
-      }),
-      _vm._v(" "),
-      _c("h3", [
-        _c("a", { attrs: { href: _vm.getUri() } }, [
-          _vm._v(_vm._s(_vm.apartment.title)),
-        ]),
+      _c("div", { staticClass: " col-sm-12 col-lg-12 my-img-container" }, [
+        _c("img", {
+          staticClass: "img-fluid",
+          attrs: {
+            src:
+              "http://127.0.0.1:8000/storage/apartments/images/" +
+              _vm.photoIds[0],
+            alt: "",
+          },
+        }),
       ]),
       _vm._v(" "),
-      _c("p", [_vm._v(_vm._s(_vm.apartment.city))]),
-      _vm._v(" "),
-      _c("p", [_vm._v(_vm._s(_vm.photoIds[0]))]),
+      _c("div", { staticClass: "col-lg-12 pt-2" }, [
+        _c("h3", [
+          _c("a", { attrs: { href: _vm.getUri() } }, [
+            _vm._v(_vm._s(_vm.apartment.title)),
+          ]),
+        ]),
+        _vm._v(" "),
+        _c("h5", [
+          _c("address", [
+            _vm._v(
+              _vm._s(_vm.apartment.address) + ", " + _vm._s(_vm.apartment.city)
+            ),
+          ]),
+        ]),
+        _vm._v(" "),
+        _c("p", [_c("strong", [_vm._v(_vm._s(_vm.apartment.price) + "  €")])]),
+      ]),
     ]
   )
 }
@@ -17005,7 +17017,7 @@ window.onresize = function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/ivannastro/Desktop/BOOLEAN/PROJECT-41/bool-bnb/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Dev\Boolean\bool-bnb\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
--- resources/js/components/apartments/ApartmentCard.vue
+++ resources/js/components/apartments/ApartmentCard.vue
@@ -18,13 +18,15 @@
             </div>
         </form>
     </div> -->
-    <div class="col-xs-12 col-sm-6 col-lg-4 mb-2 my-card ivan-card">
-        
-        <img  class="img-fluid" :src="'http://127.0.0.1:8000/storage/apartments/images/'+photoIds[0]" alt="">
-        
-        <h3><a :href="getUri()">{{apartment.title}}</a></h3>
-        <p>{{apartment.city}}</p>
-        <p>{{photoIds[0]}}</p>
+    <div class="col-xs-12 col-sm-6 col-lg-4 mb-3 my-card ivan-card">
+        <div class=" col-sm-12 col-lg-12 my-img-container">
+            <img  class="img-fluid" :src="'http://127.0.0.1:8000/storage/apartments/images/'+photoIds[0]" alt="">
+        </div>
+        <div class="col-lg-12 pt-2">
+            <h3><a :href="getUri()">{{apartment.title}}</a></h3>
+            <h5><address>{{apartment.address}}, {{apartment.city}}</address></h5>
+            <p><strong>{{apartment.price}}  &euro;</strong></p>
+        </div>
     </div>
 </template>
 
@@ -71,11 +73,16 @@ export default {
         border-radius:10px;
 
         
-        img{
-            width: 90%;
-            height: 90%;
-            border-radius: 15px;
-         
+        .my-img-container{
+            height:200px;
+            width:350px;
+            img{
+                width: 100%;
+                height: 100%;
+                background-size: cover;
+                border-radius:15px;
+                
+            }
             
         }
     
